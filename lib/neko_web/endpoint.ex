d692a2a93c9730389e8a467ffcb8e319685a4160
SukinoVERSE <me@sukino.xyz>
2026-06-01 16:01:07 +0700

    feat: add tidewave

--- lib/neko_web/endpoint.ex
+++ lib/neko_web/endpoint.ex
@@ -27,6 +27,10 @@ defmodule NekoWeb.Endpoint do
     only: NekoWeb.static_paths(),
     raise_on_missing_only: code_reloading?
 
+  if Mix.env() == :dev do
+    plug Tidewave
+  end
+
   # Code reloading can be explicitly enabled under the
   # :code_reloader configuration of your endpoint.
   if code_reloading? do
